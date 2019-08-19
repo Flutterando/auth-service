51f106fe879ce572fdd5c3bb0dd3a172740c75c4
Jacob  Moura <jacobmoura@Jacobs-iMac.local>
2019-08-18 21:25:38 -0300

    fix login

--- auth/authjwt/authjwt.go
+++ auth/authjwt/authjwt.go
@@ -1,6 +1,7 @@
 package authjwt
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -15,10 +16,13 @@ import (
 )
 
 type user struct {
-	id       int
-	name     string
-	mail     string
-	password string
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	Mail        string `json:"mail"`
+	Password    string `json:"password"`
+	Info_date   string `json:"info_date"`
+	Photo       string `json:"photo"`
+	Github_user string `json:"github_user"`
 }
 
 func (u user) createToken() string {
@@ -26,14 +30,14 @@ func (u user) createToken() string {
 
 	claims := token.Claims.(jwt.MapClaims)
 	claims["admin"] = true
-	claims["sub"] = strconv.Itoa(u.id)
-	claims["name"] = u.name
-	claims["mail"] = u.mail
+	claims["sub"] = strconv.Itoa(u.Id)
+	claims["name"] = u.Name
+	claims["mail"] = u.Mail
 	claims["iat"] = time.Now().Unix()
 	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
 
 	hasura := make(map[string]interface{})
-	hasura["x-hasura-user-id"] = strconv.Itoa(u.id)
+	hasura["x-hasura-user-id"] = strconv.Itoa(u.Id)
 	hasura["x-hasura-default-role"] = "user"
 	hasura["x-hasura-allowed-roles"] = []string{"editor", "user", "mod", "admin"}
 
@@ -57,14 +61,22 @@ var GetTokenHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Reque
 			SELECT *
 			FROM users
 			WHERE mail = $1 AND password = $2;
-		`, mail, pass).Scan(&(u.id), &(u.name), &(u.mail), &(u.password))
+		`, mail, pass).Scan(&(u.Id), &(u.Name), &(u.Mail), &(u.Password), &(u.Info_date), &(u.Photo), &(u.Github_user))
 	if err != nil {
 		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Println(err)
 		w.Write([]byte("Unauthorized\n"))
 		return
 	}
 	tokenString := u.createToken()
-	w.Write([]byte(tokenString))
+
+	jsonReturn := make(map[string]interface{})
+	jsonReturn["user"] = u
+	jsonReturn["token"] = tokenString
+
+	js, err := json.Marshal(jsonReturn)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 })
 
 // JwtMiddleware check token
